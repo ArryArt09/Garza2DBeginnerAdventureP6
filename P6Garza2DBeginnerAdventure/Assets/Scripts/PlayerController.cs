565302152699d8cd40c20b95b1ffe2a9c81beafa
ArryArt09 <746785@student.dusd.net>
2025-11-17 13:55:34 -0800

    Fixed the bindings
    I have FINALLY fixed the binding issue, and it's now way too fast. Planning to fix it with frame independence. Decorations added, collusions and finally the final hurdle will be not that far until we have to jump it.

--- P6Garza2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P6Garza2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -6,50 +6,22 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public InputAction MoveAction;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Horizontal Controls
-        float horizontal = 0.0f;
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            horizontal = -1.0f;
-        }
 
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal = 1.0f;
-        }
-
-
-        Debug.Log(horizontal);
-
-
-        // Vertical Controls
-        float vertical = 0.0f;
-        
-        if (Keyboard.current.downArrowKey.isPressed)
-        {
-            vertical = -1.0f;
-        }
-
-        else if (Keyboard.current.upArrowKey.isPressed)
-        {
-            vertical = 1.0f;
-        }
-
-        Debug.Log(vertical);
-
-        // What affects the player movement
-        Vector2 position = transform.position;
-        position.x = position.x + 0.025f * horizontal;
-        position.y = position.y + 0.025f * vertical;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.1f;
         transform.position = position;
     }
 }
