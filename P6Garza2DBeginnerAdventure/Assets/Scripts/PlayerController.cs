dc68cfa724db396a40f35b6135b0724788ff2e8e
ArryArt09 <746785@student.dusd.net>
2025-11-19 13:50:34 -0800

    Expanded Map
    I started working on my GDD and also have expanded my map, laying a proper layout and story. Focus on GDD first!

--- P6Garza2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P6Garza2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
 {
 
     public InputAction MoveAction;
+    public float speed = 3.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +22,7 @@ public class PlayerController : MonoBehaviour
 
         Vector2 move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 0.1f;
+        Vector2 position = (Vector2)transform.position + move * speed * Time.deltaTime;
         transform.position = position;
     }
 }
